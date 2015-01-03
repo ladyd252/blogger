cdbb75452da2c2800c92502a3d86a582b4d62418
Dana Yakoobinsky <dyakoobinsky@gmail.com>
2015-01-03 10:45:55 -0800

    form-based workflow feature completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def show
-    @article = Article.find(params[:id])
-  end
+
 end
